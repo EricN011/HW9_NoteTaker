0a24c9011ba3968bfc534673b51dbdf694263600
Eric Noll <eanoll88@gmail.com>
2019-12-09 19:56:54 -0600

    route commits

--- server.js
+++ server.js
@@ -2,3 +2,21 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const server = express();
+
+// use express to handle data
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+// Data
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+server.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+// Listener
+server.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
