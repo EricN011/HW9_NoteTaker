e7799a765c603d515687a34794dcfb63a4677a4b
Eric Noll <eanoll88@gmail.com>
2019-12-09 20:04:36 -0600

    reading data

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
-const server = express();
+const app = express();
 
 // use express to handle data
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
+app.use(express.static("css"));
+app.use(express.static("js"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Data
-server.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.get("/notes", (req, res) => {
+app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
 
+app.get("/api/notes", (req, res) => {
+  fs.readFile("db.json", (err, data) => {
+    if (err) throw err;
+    database = JSON.parse(data);
+    return res.json(database[0].notes);
+  });
+});
+
 // Listener
-server.listen(PORT, function() {
+app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
